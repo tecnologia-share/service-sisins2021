b8b711003eaf3cddad1d7c4d87cb7b46ca19b6e9
Renan Oliveira <48491038+Renan04lima@users.noreply.github.com>
2021-10-11 09:24:50 -0300

    Revert "fix: update config babel"

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,6 @@ module.exports = {
     '@babel/preset-typescript',
   ],
   plugins: [
-    ['module-resolver'],
     'babel-plugin-transform-typescript-metadata',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
