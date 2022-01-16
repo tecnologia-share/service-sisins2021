80441e5984937e9e9c0a6c8cf338838d658d54e2
renan04lim <renan04lima@outlook.com>
2022-01-16 11:57:14 -0300

    refactor: move dotenv from server.ts to env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,14 +1,15 @@
 import { get } from 'env-var';
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
 
-const PORT = 3333;
 export const env = {
   smtpUser: get('SMTP_USER').required().asString(),
   smtpPassword: get('SMTP_PASSWORD').required().asString(),
   smtpPort: get('SMTP_PORT').required().asString(),
   smtpHost: get('SMTP_HOST').required().asString(),
   email: get('EMAIL').required().asString(),
-  port: get('PORT').default(PORT).asPortNumber(),
-  hostBack: get('HOST_BACKEND').default(`http://localhost:${PORT}`).asString(),
+  port: get('PORT').default(3333).asPortNumber(),
+  hostBack: get('HOST_BACKEND').default(`http://localhost:${3333}`).asString(),
   hostFront: get('HOST_FRONTEND').default('http://localhost').asString(),
 };
 
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,5 @@
-import dotenv from 'dotenv';
-import { env } from '../../../config/env';
+import { env } from '@/config/env';
 import app from './app';
-dotenv.config({ path: '.env' });
 
 const port = env.port;
 app.listen(port, () => {
